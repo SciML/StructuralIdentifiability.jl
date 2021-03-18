5f7cc21b6e9fa9f6048798590b0be41182190efa
pogudingleb <pogudin.gleb@gmail.com>
2021-03-18 14:21:11 +0300

    Package files created

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,16 +6,15 @@ using Nemo
 using Oscar
 using Singular
 
-include("../src/StructuralIdentifiability.jl")
-using .StructuralIdentifiability: check_field_membership, check_identifiability, check_primality_zerodim,
-                                  det_minor_expansion, ExpVectTrie, get_max_below, ps_ode_solution, 
-                                  power_series_solution, ps_diff, ps_integrate, ps_matrix_inv,
-                                  ps_matrix_homlinear_de, ps_matrix_linear_de, ps_matrix_log,
-                                  reduce_ode_mod_p, simplify_field_generators, ODE, @ODEmodel,
-                                  truncate_matrix, find_ioequations, str_to_var, unpack_fraction,
-                                  assess_global_identifiability, differentiate_output, var_to_str,
-                                  switch_ring, eval_at_dict, assess_local_identifiability,
-                                  assess_identifiability
+using StructuralIdentifiability: check_field_membership, check_identifiability, check_primality_zerodim,
+                                 det_minor_expansion, ExpVectTrie, get_max_below, ps_ode_solution, 
+                                 power_series_solution, ps_diff, ps_integrate, ps_matrix_inv,
+                                 ps_matrix_homlinear_de, ps_matrix_linear_de, ps_matrix_log,
+                                 reduce_ode_mod_p, simplify_field_generators, ODE, @ODEmodel,
+                                 truncate_matrix, find_ioequations, str_to_var, unpack_fraction,
+                                 assess_global_identifiability, differentiate_output, var_to_str,
+                                 switch_ring, eval_at_dict, assess_local_identifiability,
+                                 assess_identifiability
 
 function random_ps(ps_ring, range = 1000)
     result = zero(ps_ring)
