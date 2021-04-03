06103d00e249e7651dff042908f8a297272eb85b
pogudingleb <pogudin.gleb@gmail.com>
2021-04-03 16:53:50 +0300

    benchmarks: Crauste&Fujita

--- benchmarking/benchmarks.jl
+++ benchmarking/benchmarks.jl
@@ -102,5 +102,35 @@ benchmarks = [
             y1(t) = i(t),
             y2(t) = r(t)
         )
+    ),
+    Dict(
+        :name => "Akt pathway",
+        :ode => @ODEmodel(
+            EGFR'(t) = EGFR_turnover * pro_EGFR(t) + EGF_EGFR(t) * reaction_1_k2 - EGFR(t) * EGFR_turnover - EGF_EGFR(t) * reaction_1_k1,
+            pEGFR'(t) = EGF_EGFR(t) * reaction_9_k1 - pEGFR(t) * reaction_4_k1 + pEGFR_Akt(t) * reaction_2_k2 + pEGFR_Akt(t) * reaction_3_k1 - Akt(t) * pEGFR(t) * reaction_2_k1,
+            pEGFR_Akt'(t) = Akt(t) * pEGFR(t) * reaction_2_k1 - pEGFR_Akt(t) * reaction_3_k1 - pEGFR_Akt(t) * reaction_2_k2,
+            Akt'(t) = pAkt(t) * reaction_7_k1 + pEGFR_Akt(t) * reaction_2_k2 - Akt(t) * pEGFR(t) * reaction_2_k1,
+            pAkt'(t) = pAkt_S6(t) * reaction_5_k2 - pAkt(t) * reaction_7_k1 + pAkt_S6(t) * reaction_6_k1 + pEGFR_Akt(t) * reaction_3_k1 - S6(t) * pAkt(t) * reaction_5_k1,
+            S6'(t) = pAkt_S6(t) * reaction_5_k2 + pS6(t) * reaction_8_k1 - S6(t) * pAkt(t) * reaction_5_k1,
+            pAkt_S6'(t) = S6(t) * pAkt(t) * reaction_5_k1 - pAkt_S6(t) * reaction_6_k1 - pAkt_S6(t) * reaction_5_k2,
+            pS6'(t) = pAkt_S6(t) * reaction_6_k1 - pS6(t) * reaction_8_k1,
+            EGF_EGFR'(t) = EGF_EGFR(t) * reaction_1_k1 - EGF_EGFR(t) * reaction_9_k1 - EGF_EGFR(t) * reaction_1_k2,
+            y1(t) = a1 * (pEGFR(t) + pEGFR_Akt(t)),
+            y2(t) = a2 * (pAkt(t) + pAkt_S6(t)),
+            y3(t) = a3 * pS6(t)
+        )
+    ),
+    Dict(
+        :name => "CD8 T cell differentiation",
+        :ode => @ODEmodel(
+            N'(t) = - N(t) * mu_N - N(t) * P(t) * delta_NE,
+            E'(t) = N(t) * P(t) * delta_NE - E(t)^2 * mu_EE - E(t) * delta_EL + E(t) * P(t) * rho_E,
+            S'(t) = S(t) * delta_EL - S(t) * delta_LM - S(t)^2 * mu_LL - E(t) * S(t) * mu_LE,
+            M'(t) = S(t) * delta_LM - mu_M * M(t),
+            P'(t) = P(t)^2 * rho_P - P(t) * mu_P - E(t) * P(t) * mu_PE - S(t) * P(t) * mu_PL,
+            y1(t) = N(t),
+            y2(t) = E(t) + S(t),
+            y3(t) = M(t)
+        )
     )
 ]
