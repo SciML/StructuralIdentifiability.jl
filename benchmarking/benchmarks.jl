be24f23261722fe5a30a8c69a49ee342c934c405
Gleb Pogudin <pogudin@ron.medicis.polytechnique.fr>
2021-04-05 00:30:40 +0200

    benchmarks updated

--- benchmarking/benchmarks.jl
+++ benchmarking/benchmarks.jl
@@ -31,7 +31,7 @@ benchmarks = [
             x3'(t) = b2 * (x2(t) - x3(t)),
             y1(t) = x0(t)
         ),
-        :skip => true
+        :skip => false
     ),
     Dict(
         :name => "SEAIJRC Covid model",
@@ -42,10 +42,11 @@ benchmarks = [
             I'(t) = k * r * E(t) - (alpha + g1) * I(t),
             J'(t) = alpha * I(t) - g2 * J(t),
             C'(t) = alpha * I(t),
+            Ninv'(t) = 0,
             y(t) = C(t),
             y2(t) = Ninv(t)
         ),
-        :skip => true
+        :skip => false
     ),
     Dict(
         :name => "MAPK model (5 outputs)",
@@ -62,6 +63,51 @@ benchmarks = [
             S01'(t) = alpha11 * F(t) * S11(t) - (beta11 + gamma1101 + gamma1110 + gamma1100) * FS11(t),
             S10'(t) = -a00 * K(t) * S00(t) + (b00 + c0001 + c0010 + c0011) * KS00(t) - a01 * K(t) * S01(t) + (b01 + c0111) * KS01(t) - a10 * K(t) * S10(t) + (b10 + c1011) * KS10(t),
             S11'(t) = -alpha01 * F(t) * S01(t) + (beta01 + gamma0100) * FS01(t) - alpha10 * F(t) * S10(t) + (beta10 + gamma1000) * FS10(t) - alpha11 * F(t) * S11(t) + (beta11 + gamma1101 + gamma1110 + gamma1100) * FS11(t),
+            y1(t) = F(t),
+            y2(t) = S00(t),
+            y3(t) = S01(t),
+            y4(t) = S10(t),
+            y5(t) = S11(t)
+        ) 
+    ),
+    Dict(
+        :name => "MAPK model (5 outputs bis)",
+        :ode => @ODEmodel(
+            KS00'(t) = -a00 * K(t) * S00(t) + b00 * KS00(t) + gamma0100 * FS01(t) + gamma1000 * FS10(t) + gamma1100 * FS11(t),
+            KS01'(t) = -a01 * K(t) * S01(t) + b01 * KS01(t) + c0001 * KS00(t) - alpha01 * F(t) * S01(t) + beta01 * FS01(t) + gamma1101 * FS11(t),
+            KS10'(t) = -a10 * K(t) * S10(t) + b10 * KS10(t) + c0010 * KS00(t) - alpha10 * F(t) * S10(t) + beta10 * FS10(t) + gamma1110 * FS11(t),
+            FS01'(t) = -alpha11 * F(t) * S11(t) + beta11 * FS11(t) + c0111 * KS01(t) + c1011 * KS10(t) + c0011 * KS00(t),
+            FS10'(t) = a00 * K(t) * S00(t) - (b00 + c0001 + c0010 + c0011) * KS00(t),
+            FS11'(t) = a01 * K(t) * S01(t) - (b01 + c0111) * KS01(t),
+            K'(t) = a10 * K(t) * S10(t) - (b10 + c1011) * KS10(t),
+            F'(t) = alpha01 * F(t) * S01(t) - (beta01 + gamma0100) * FS01(t),
+            S00'(t) = alpha10 * F(t) * S10(t) - (beta10 + gamma1000) * FS10(t),
+            S01'(t) = alpha11 * F(t) * S11(t) - (beta11 + gamma1101 + gamma1110 + gamma1100) * FS11(t),
+            S10'(t) = -a00 * K(t) * S00(t) + (b00 + c0001 + c0010 + c0011) * KS00(t) - a01 * K(t) * S01(t) + (b01 + c0111) * KS01(t) - a10 * K(t) * S10(t) + (b10 + c1011) * KS10(t),
+            S11'(t) = -alpha01 * F(t) * S01(t) + (beta01 + gamma0100) * FS01(t) - alpha10 * F(t) * S10(t) + (beta10 + gamma1000) * FS10(t) - alpha11 * F(t) * S11(t) + (beta11 + gamma1101 + gamma1110 + gamma1100) * FS11(t),
+            y0(t) = K(t),
+            y1(t) = F(t),
+            y2(t) = S00(t),
+            y3(t) = S01(t) + S10(t),
+            y4(t) = S11(t)
+        ) 
+    ),
+    Dict(
+        :name => "MAPK model (6 outputs)",
+        :ode => @ODEmodel(
+            KS00'(t) = -a00 * K(t) * S00(t) + b00 * KS00(t) + gamma0100 * FS01(t) + gamma1000 * FS10(t) + gamma1100 * FS11(t),
+            KS01'(t) = -a01 * K(t) * S01(t) + b01 * KS01(t) + c0001 * KS00(t) - alpha01 * F(t) * S01(t) + beta01 * FS01(t) + gamma1101 * FS11(t),
+            KS10'(t) = -a10 * K(t) * S10(t) + b10 * KS10(t) + c0010 * KS00(t) - alpha10 * F(t) * S10(t) + beta10 * FS10(t) + gamma1110 * FS11(t),
+            FS01'(t) = -alpha11 * F(t) * S11(t) + beta11 * FS11(t) + c0111 * KS01(t) + c1011 * KS10(t) + c0011 * KS00(t),
+            FS10'(t) = a00 * K(t) * S00(t) - (b00 + c0001 + c0010 + c0011) * KS00(t),
+            FS11'(t) = a01 * K(t) * S01(t) - (b01 + c0111) * KS01(t),
+            K'(t) = a10 * K(t) * S10(t) - (b10 + c1011) * KS10(t),
+            F'(t) = alpha01 * F(t) * S01(t) - (beta01 + gamma0100) * FS01(t),
+            S00'(t) = alpha10 * F(t) * S10(t) - (beta10 + gamma1000) * FS10(t),
+            S01'(t) = alpha11 * F(t) * S11(t) - (beta11 + gamma1101 + gamma1110 + gamma1100) * FS11(t),
+            S10'(t) = -a00 * K(t) * S00(t) + (b00 + c0001 + c0010 + c0011) * KS00(t) - a01 * K(t) * S01(t) + (b01 + c0111) * KS01(t) - a10 * K(t) * S10(t) + (b10 + c1011) * KS10(t),
+            S11'(t) = -alpha01 * F(t) * S01(t) + (beta01 + gamma0100) * FS01(t) - alpha10 * F(t) * S10(t) + (beta10 + gamma1000) * FS10(t) - alpha11 * F(t) * S11(t) + (beta11 + gamma1101 + gamma1110 + gamma1100) * FS11(t),
+            y0(t) = K(t),
             y1(t) = F(t),
             y2(t) = S00(t),
             y3(t) = S01(t),
