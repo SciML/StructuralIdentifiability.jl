53e565938a2b7fa078f96b356787faa24f2663cc
pogudingleb <pogudin.gleb@gmail.com>
2021-07-06 17:42:32 +0200

    Tests: invertibility ensured

--- test/ps_matrix_homlinear.jl
+++ test/ps_matrix_homlinear.jl
@@ -8,6 +8,9 @@
             A = random_ps_matrix(T, S)
             Sconst = Nemo.MatrixSpace(Nemo.GF(2^31 - 1), d, d)
             Y0 = Sconst([rand(Int) % 100 for i = 1:d, j = 1:d])
+            while StructuralIdentifiability.LinearAlgebra.det(Y0) == 0
+                Y0 = Sconst([rand(Int) % 100 for i = 1:d, j = 1:d])
+            end
             @time sol, sol_inv = ps_matrix_homlinear_de(A, Y0)
             to_be_zero = map(ps_diff, sol) - A * sol
             @test truncate_matrix(to_be_zero, 299) == truncate_matrix(zero(S), 299)
