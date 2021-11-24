33c73c75eb9689059395d629b0b8567c0a528ec9
Ilia Ilmer <i.ilmer@icloud.com>
2021-11-23 20:27:46 -0500

    adding oscar version

--- src/global_identifiability.jl
+++ src/global_identifiability.jl
@@ -82,7 +82,7 @@ function check_field_membership(
     if method == :Singular
         gb = Singular.std(Singular.Ideal(ring_sing, eqs_sing))
     elseif method == :GroebnerBasis
-        gb = Oscar.groebner_basis(Oscar.MPolyIdeal(ring_sing, eqs_sing))
+        gb = f4Oscar.MPolyIdeal(ring_sing, eqs_sing))
     else
         throw(Base.ArgumentError("Unknown method $method"))
     end
