74ded4d7067666b9f56e9f4b06d8af4a7a67fbdd
Krishna Bhogaonker <cyclotomiq@gmail.com>
2022-11-12 19:20:46 +0000

    added reproducibility to index.md and make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,9 @@
 using Documenter, StructuralIdentifiability
 using BenchmarkTools
 
+cp("./docs/Manifest.toml", "./docs/src/assets/Manifest.toml", force = true)
+cp("./docs/Project.toml", "./docs/src/assets/Project.toml", force = true)
+
 include("pages.jl")
 
 makedocs(
