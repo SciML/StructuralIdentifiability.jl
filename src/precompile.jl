4a481b2f5d6adabdc4201636b980ccbbb858bf54
pogudingleb <pogudin.gleb@gmail.com>
2023-07-01 21:49:57 +0200

    no logging at precompilation + MTK interface in precompilation

--- src/precompile.jl
+++ src/precompile.jl
@@ -3,6 +3,12 @@
     # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
     # precompile file and potentially make loading faster.
     using Logging
+    using ModelingToolkit
+    @parameters a01 a21 a12
+    @variables t x0(t) x1(t) y(t)
+    D = Differential(t)
+    eqs = [D(x0) ~ -(a01 + a21) * x0 + a12 * x1, D(x1) ~ a21 * x0 - a12 * x1]
+    de = ODESystem(eqs, t, name = :Test)
     @compile_workload begin
         # all calls in this block will be precompiled, regardless of whether
         # they belong to your package or not (on Julia 1.8 and higher)
@@ -14,6 +20,8 @@
                 y(t) = x2(t)
             )
             assess_identifiability(ode)
+            assess_identifiability(de; measured_quantities = [x0])
+            assess_identifiability(de; measured_quantities = [y ~ x0])
         end
     end
 end
