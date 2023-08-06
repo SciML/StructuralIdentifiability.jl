eaf303e92c7d43b37b2b9e51c9141f685b26e615
Sasha Demin <ademin@mpi-inf.mpg.de>
2023-08-06 20:40:48 +0200

    now example works in master branch

--- benchmarking/IdentifiableFunctions/peculiarity.jl
+++ benchmarking/IdentifiableFunctions/peculiarity.jl
@@ -5,7 +5,30 @@ R, (k01, k31, k21, k12, k13, k14, k41) =
     QQ[["k01", "k31", "k21", "k12", "k13", "k14", "k41"]...]
 
 # Note: 7 elements
-f1 = [
+f1_lists = [
+    [R(1), k01],
+    [R(1), k12 + k13 + k14],
+    [R(1), k31 + k21 + k41],
+    [
+        k31 * k12 - k31 * k14 - k21 * k13 + k21 * k14 - k12 * k41 + k13 * k41,
+        k12^2 * k13 - k12^2 * k14 - k12 * k13^2 + k12 * k14^2 + k13^2 * k14 - k13 * k14^2,
+    ],
+    [
+        k31 * k12 - k31 * k14 - k21 * k13 + k21 * k14 - k12 * k41 + k13 * k41,
+        k31 * k12 * k13 - k31 * k13 * k14 - k21 * k12 * k13 + k21 * k12 * k14 -
+        k12 * k14 * k41 + k13 * k14 * k41,
+    ],
+    [
+        k31 * k12 - k31 * k14 - k21 * k13 + k21 * k14 - k12 * k41 + k13 * k41,
+        k31 * k21 * k12 - k31 * k21 * k13 + k31 * k13 * k41 - k31 * k14 * k41 -
+        k21 * k12 * k41 + k21 * k14 * k41,
+    ],
+    [
+        k31 * k12 - k31 * k14 - k21 * k13 + k21 * k14 - k12 * k41 + k13 * k41,
+        k31^2 * k21 - k31^2 * k41 - k31 * k21^2 + k31 * k41^2 + k21^2 * k41 - k21 * k41^2,
+    ],
+]
+f1_fracs = [
     k01,
     k12 + k13 + k14,
     k31 + k21 + k41,
@@ -22,27 +45,43 @@ f1 = [
 ]
 
 # Note: 8 elements
-f2 = [
-    k01,
-    k12 * k13 * k14,
-    k31 * k21 * k41,
-    k12 + k13 + k14,
-    k31 + k21 + k41,
-    k12 * k13 + k12 * k14 + k13 * k14,
-    k31 * k13 + k21 * k12 + k14 * k41,
-    k31 * k21 + k31 * k41 + k21 * k41,
+f2_lists = [
+    [R(1), k01],
+    [R(1), k12 * k13 * k14],
+    [R(1), k31 * k21 * k41],
+    [R(1), k12 + k13 + k14],
+    [R(1), k31 + k21 + k41],
+    [R(1), k12 * k13 + k12 * k14 + k13 * k14],
+    [R(1), k31 * k13 + k21 * k12 + k14 * k41],
+    [R(1), k31 * k21 + k31 * k41 + k21 * k41],
+]
+f2_fracs = [
+    (k01) // R(1),
+    (k12 * k13 * k14) // R(1),
+    (k31 * k21 * k41) // R(1),
+    (k12 + k13 + k14) // R(1),
+    (k31 + k21 + k41) // R(1),
+    (k12 * k13 + k12 * k14 + k13 * k14) // R(1),
+    (k31 * k13 + k21 * k12 + k14 * k41) // R(1),
+    (k31 * k21 + k31 * k41 + k21 * k41) // R(1),
 ]
 
-# Prints: 1, 1
-i12 = StructuralIdentifiability.check_field_membership(f1, f2, 0.9999)
-i21 = StructuralIdentifiability.check_field_membership(f2, f1, 0.9999)
+@assert length(f1_lists) == length(f1_fracs) == 7
+@assert length(f2_lists) == length(f2_fracs) == 8
+
+# Prints: true; true
+i12 = StructuralIdentifiability.check_field_membership(f1_lists, f2_fracs, 0.9999)
+i21 = StructuralIdentifiability.check_field_membership(f2_lists, f1_fracs, 0.9999)
 @info "" i12 i21
 
-# Prints: 0, 0, ..., 0
-res = [false for _ in 1:length(f2)]
-for i in 1:length(f2)
-    all_except_i = f2[setdiff(1:length(f2), i)]
-    res[i] =
-        StructuralIdentifiability.check_field_membership(all_except_i, [f2[i]], 0.9999)[1]
+# Prints: false, false, ..., false
+res = [false for _ in 1:length(f2_lists)]
+for i in 1:length(f2_lists)
+    all_except_i = f2_lists[setdiff(1:length(f2_lists), i)]
+    res[i] = StructuralIdentifiability.check_field_membership(
+        all_except_i,
+        [f2_fracs[i]],
+        0.9999,
+    )[1]
 end
 @info "" res
