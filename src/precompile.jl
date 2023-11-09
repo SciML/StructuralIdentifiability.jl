ad5d6865467b2f00b89ced9ec4346d525e6fe9f8
pogudingleb <pogudin.gleb@gmail.com>
2023-11-10 00:54:43 +0100

    adding scope

--- src/precompile.jl
+++ src/precompile.jl
@@ -10,18 +10,20 @@
     eqs = [D(x0) ~ -(a01 + a21) * x0 + a12 * x1, D(x1) ~ a21 * x0 - a12 * x1]
     de = ODESystem(eqs, t, name = :Test)
     @compile_workload begin
-        # all calls in this block will be precompiled, regardless of whether
-        # they belong to your package or not (on Julia 1.8 and higher)
-        restart_logging(loglevel = Logging.Warn)
-        ode = @ODEmodel(
-            x1'(t) = -(a01 + a21) * x1(t) + a12 * x2(t) + u(t),
-            x2'(t) = a21 * x1(t) - a12 * x2(t) - x3(t) / b,
-            x3'(t) = x3(t),
-            y(t) = x2(t)
-        )
-        assess_identifiability(ode)
-        assess_identifiability(de; measured_quantities = [x0])
-        assess_identifiability(de; measured_quantities = [y ~ x0])
-        find_identifiable_functions(ode, with_states = true)
+        with_logger(Logging.ConsoleLogger(Logging.Warn)) do
+            # all calls in this block will be precompiled, regardless of whether
+            # they belong to your package or not (on Julia 1.8 and higher)
+            restart_logging(loglevel = Logging.Warn)
+            ode = @ODEmodel(
+                x1'(t) = -(a01 + a21) * x1(t) + a12 * x2(t) + u(t),
+                x2'(t) = a21 * x1(t) - a12 * x2(t) - x3(t) / b,
+                x3'(t) = x3(t),
+                y(t) = x2(t)
+            )
+            assess_identifiability(ode)
+            assess_identifiability(de; measured_quantities = [x0])
+            assess_identifiability(de; measured_quantities = [y ~ x0])
+            find_identifiable_functions(ode, with_states = true)
+        end
     end
 end
