e720f37731eca21667a4746077524ac77552f78f
pogudingleb <pogudin.gleb@gmail.com>
2023-11-22 00:53:00 +0100

    assess_identifiability_kic but no tests yet

--- src/known_ic.jl
+++ src/known_ic.jl
@@ -69,7 +69,6 @@ function _find_identifiable_functions_kic(
         rational_interpolator = rational_interpolator,
         seed = seed,
     )
-    @info "The search for identifiable functions with known initial conditions concluded in $((time_ns() - runtime_start) / 1e9) seconds"
 
     id_funcs = simplified_generating_set(
         RationalFunctionField(vcat(id_funcs_general, [f // one(parent(ode)) for f in known_ic])),
@@ -79,7 +78,80 @@ function _find_identifiable_functions_kic(
         rational_interpolator = rational_interpolator,
     )
 
+    @info "The search for identifiable functions with known initial conditions concluded in $((time_ns() - runtime_start) / 1e9) seconds"
+
     return id_funcs
+end
+
+"""
+    assess_identifiability_kic(ode, known_ic; funcs_to_check = [], p=0.99, loglevel=Logging.Info)
+
+Input:
+- `ode` - the ODE model
+- `known_ic` - a list of functions for which initial conditions are assumed to be known and generic
+- `funcs_to_check` - list of functions to check identifiability for; if empty, all parameters
+   and states are taken
+- `p` - probability of correctness.
+- `loglevel` - the minimal level of log messages to display (`Logging.Info` by default)
+
+Assesses identifiability of parameters and initial conditions of a given ODE model. 
+The result is guaranteed to be correct with the probability at least `p`.
+The function returns an (ordered) dictionary from the functions to check to their identifiability properties 
+(one of `:nonidentifiable`, `:locally`, `:globally`).
+"""
+function assess_identifiability_kic(
+    ode::ODE{P},
+    known_ic::Vector{<: Union{P, Generic.Frac{P}}};
+    funcs_to_check = Vector(),
+    p::Float64 = 0.99,
+    loglevel = Logging.Info,
+) where {P <: MPolyElem{fmpq}}
+    restart_logging(loglevel = loglevel)
+    reset_timings()
+    with_logger(_si_logger[]) do
+        return _assess_identifiability_kic(
+            ode,
+            known_ic,
+            p = p,
+            funcs_to_check = funcs_to_check,
+        )
+    end
+end
+
+function _assess_identifiability_kic(
+    ode::ODE{P},
+    known_ic::Vector{<: Union{P, Generic.Frac{P}}};
+    funcs_to_check = Vector(),
+    p::Float64 = 0.99,
+) where {P <: MPolyElem{fmpq}}
+    runtime_start = time_ns()
+    if length(funcs_to_check) == 0
+        funcs_to_check = vcat(ode.x_vars, ode.parameters)
+    end
+    half_p = 0.5 + p / 2
+    id_funcs = _find_identifiable_functions_kic(ode, known_ic, p = half_p)
+    result = OrderedDict(f => :globally for f in funcs_to_check)
+
+    half_p = 0.5 + p / 2
+    local_result = check_algebraicity(
+        RationalFunctionField([[denominator(f), numerator(f)] for f in id_funcs]),
+        [f // one(parent(f)) for f in funcs_to_check],
+        half_p,
+    )
+    global_result = field_contains(
+        RationalFunctionField([[denominator(f), numerator(f)] for f in id_funcs]),
+        [f // one(parent(f)) for f in funcs_to_check],
+        half_p,
+    )
+    for (i, f) in enumerate(funcs_to_check)
+        if !local_result[i]
+            result[f] = :nonidentifiable
+        elseif !global_result[i]
+            result[f] = :locally
+        end
+    end
+    @info "Assessing identifiability with known initial conditions concluded in $((time_ns() - runtime_start) / 1e9) seconds"
+    return result 
 end
 
 
