52dd8c3ae67fd4fcff86b992ccab778100c11569
pogudingleb <pogudin.gleb@gmail.com>
2024-01-18 09:51:16 +0100

    optionalizing MTK in the tests

--- test/ps_matrix_linear.jl
+++ test/ps_matrix_linear.jl
@@ -7,7 +7,7 @@
             S = Nemo.MatrixSpace(T, d, d)
             A = random_ps_matrix(T, S)
             B = random_ps_matrix(T, S)
-            Sconst = Nemo.MatrixSpace(Nemo.GF(2^31 - 1), d, d)
+            Sconst = Nemo.MatrixSpace(Nemo.Native.GF(2^31 - 1), d, d)
             Y0 = Sconst([rand(Int) % 100 for i in 1:d, j in 1:d])
             @time sol = ps_matrix_linear_de(A, B, Y0)
             to_be_zero = map(ps_diff, sol) - A * sol - B
