7479ec477da0f550fa89adc675095a82f7bb2376
pogudingleb <pogudin.gleb@gmail.com>
2024-01-31 15:58:01 +0100

    printing

--- test/qa.jl
+++ test/qa.jl
@@ -1,24 +1,22 @@
 using StructuralIdentifiability, Aqua
+using TOML
 import Aqua
 
-function Aqua.find_persistent_tasks_deps(a::Int, package::PkgId; kwargs...)
-    root_project_path, found = root_project_toml(package)
+function Aqua.find_persistent_tasks_deps(a::Int, package::Base.PkgId; kwargs...)
+    root_project_path, found = Aqua.root_project_toml(package)
     found || error("Unable to locate Project.toml")
     prj = TOML.parsefile(root_project_path)
     deps = get(prj, "deps", Dict{String,Any}())
     filter!(deps) do (name, uuid)
-        id = PkgId(UUID(uuid), name)
+        id = Base.PkgId(Base.UUID(uuid), name)
         println("Checking $name")
-        return has_persistent_tasks(id; kwargs...)
+        return Aqua.has_persistent_tasks(id; kwargs...)
     end
     return [name for (name, _) in deps]
 end
 
-
-
-
 @testset "Aqua" begin
-    Aqua.find_persistent_tasks_deps(42, StructuralIdentifiability)
+    Aqua.find_persistent_tasks_deps(42, Base.PkgId(StructuralIdentifiability))
     Aqua.test_ambiguities(StructuralIdentifiability, recursive = false)
     Aqua.test_deps_compat(StructuralIdentifiability)
     Aqua.test_piracies(StructuralIdentifiability, treat_as_own = [])
