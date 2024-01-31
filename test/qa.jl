0237dbd485fe7573b272724a968275c059e62380
pogudingleb <pogudin.gleb@gmail.com>
2024-01-31 15:24:30 +0100

    removing other actions and printing package names

--- test/qa.jl
+++ test/qa.jl
@@ -1,6 +1,24 @@
 using StructuralIdentifiability, Aqua
+import Aqua
+
+function Aqua.find_persistent_tasks_deps(a::Int, package::PkgId; kwargs...)
+    root_project_path, found = root_project_toml(package)
+    found || error("Unable to locate Project.toml")
+    prj = TOML.parsefile(root_project_path)
+    deps = get(prj, "deps", Dict{String,Any}())
+    filter!(deps) do (name, uuid)
+        id = PkgId(UUID(uuid), name)
+        println("Checking $name")
+        return has_persistent_tasks(id; kwargs...)
+    end
+    return [name for (name, _) in deps]
+end
+
+
+
+
 @testset "Aqua" begin
-    Aqua.find_persistent_tasks_deps(StructuralIdentifiability)
+    Aqua.find_persistent_tasks_deps(42, StructuralIdentifiability)
     Aqua.test_ambiguities(StructuralIdentifiability, recursive = false)
     Aqua.test_deps_compat(StructuralIdentifiability)
     Aqua.test_piracies(StructuralIdentifiability, treat_as_own = [])
