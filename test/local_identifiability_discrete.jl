1ac69dfe1c40f49716fa3e85f87d5ac504146123
pogudingleb <gleb.pogudin@iMac-de-Gleb.local>
2024-06-10 14:57:47 +0200

    moving the tests with inputs to the DDSmodel interface

--- test/local_identifiability_discrete.jl
+++ test/local_identifiability_discrete.jl
@@ -23,8 +23,6 @@
 
     #---------------------
 
-    cases = []
-
     dds = @DDSmodel(x1(t + 1) = a * x1(t) * x2(t), x2(t + 1) = b * x1(t), y(t) = x2(t))
 
     push!(
@@ -47,6 +45,68 @@
     )
 
     #---------------------
+
+    # Example 1 from https://doi.org/10.1016/j.automatica.2008.03.019
+    dds = @DDSmodel(
+        x1(t + 1) = theta1 * x1(t) + x2(t), 
+        x2(t + 1) = (1 - theta2) * x1(t) + x2(t)^2 + u(t),
+        y(t) = x1(t)
+    )
+
+    push!(
+        cases,
+        Dict(
+            :dds => dds,
+            :res => OrderedDict(x1 => true, x2 => true, theta1 => true, theta2 => true),
+            :known => :none,
+        ),
+    )
+
+    # Example 2 from https://doi.org/10.1016/j.automatica.2008.03.019
+    dds = @DDSmodel(
+        x1(t + 1) = theta1 * x1(t)^2 + theta2 * x2(t) + u(t), 
+        x2(t + 1) = theta3 * x1(t),
+        y(t) = x1(t)
+    )
+
+    push!(
+        cases,
+        Dict(
+            :dds => dds,
+            :res => OrderedDict(
+                x1 => true,
+                x2 => false,
+                theta1 => true,
+                theta2 => false,
+                theta3 => false,
+            ),
+            :known => :none,
+        ),
+    )
+
+    dds = @DDSmodel(
+        x1(t + 1) = theta1 * x1(t)^2 + theta2 * x2(t) + u(t), 
+        x2(t + 1) = theta3 * x1(t),
+        y(t) = x1(t),
+        y2(t) = x2(t)
+    )
+    push!(
+        cases,
+        Dict(
+            :dds => dds,
+            :res => Dict(
+                x1 => true,
+                x2 => true,
+                theta1 => true,
+                theta2 => true,
+                theta3 => true,
+            ),
+            :known => :none,
+        ),
+    )
+ 
+    #---------------------
+
 
     for c in cases
         @test assess_local_identifiability(
