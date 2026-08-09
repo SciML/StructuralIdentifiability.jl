e599e0bddc67251d20dda0c14ef691615b554f9c
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-08-08 20:35:54 -0400

    Enforce strict QA and doctested docs
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- src/lincomp.jl
+++ src/lincomp.jl
@@ -33,6 +33,8 @@ In the list above, the `i`-th element is a list of vertices to which there exist
 from the vertex `i`. Now we can create a linear compartment model over this graph with
 the output at vertex 1, input at vertex 2, and leaks at vertices 3 and 4 as follows:
 ```jldoctest
+julia> using StructuralIdentifiability
+
 julia> ode = linear_compartment_model([[2, 4], [1, 3], [2, 4], [1, 3]], outputs = [1], inputs = [2], leaks = [2, 3])
 x1' = -x1*a_2_1 - x1*a_4_1 + x2*a_1_2 + x4*a_1_4
 x3' = x2*a_3_2 - x3*a_2_3 - x3*a_4_3 - x3*a_0_3 + x4*a_3_4
